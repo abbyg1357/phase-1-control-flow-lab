a304b49826e20cce959adbc1357fea26ac67bfa7
Abigail <abbyg1357@gmail.com>
2024-03-07 08:34:33 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let result;
+  if(someValue <= 400) {
+    result = 'This one is on me!';
+  }
+  else if(someValue <= 2000) {
+    result = 'That will be twenty bucks.';
+  }
+  else if(someValue <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if(someValue > 2500) {
+    result = 'No can do.';
+  }
+   return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC'?'Ok, sounds good.' : `No go.`;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous) {
+ 
+ switch (generous) {
+  case 'generous':
+    return ('Thank you so much.');
+    break;
+  case 'not as generous':
+    return ('Thank you.');
+    break;
+  default:
+    return 'Bye.'
+    break;
+ 
+  }
 }
